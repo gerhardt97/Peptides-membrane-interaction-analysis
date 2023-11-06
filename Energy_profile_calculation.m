5ec53862882baa1300a5ce0aec53302fd2aaf6de
YANG <147957794+gerhardt97@users.noreply.github.com>
2023-11-07 00:44:03 +0800

    Energy_profile_calculation.m

--- Energy_profile_calculation.m
+++ Energy_profile_calculation.m
@@ -1,12 +1,13 @@
-clc, clear all
+% This code was used for analyze the required energy to overcome the energy barrier of GUVs after FCLAPs treatment
 
+clc, clear all
 [x]=xlsread('C:\Users\yzh74\Optical Tweezers Results Analysis\trap_position.csv') 
 [y]=xlsread('C:\Users\yzh74\Optical Tweezers Results Analysis\force.csv') 
 
 % Creating spline function to represent F-D curve
 xx=linspace(x(1),x(max),100);
 yy=spline(x,-y,xx);
-%%
+
 % Calculating energy profile from the integral of the force-distance curve
 G1 = []
 for i = 1:100
@@ -15,7 +16,7 @@ for i = 1:100
     G1 = [G1,G]
 end
 disp(G1)
-%%
+
 % Plot F-D curve and G-D curve
 plot(xx,yy)
 hold on
